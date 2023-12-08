b40bba749c1061ac60f1c8e2bd0b3e01d14a5dc3
Zakriahassan44 <kzakria576@gmail.com>
2023-12-08 14:19:40 -0800

    flutter culculator

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const GrayColor = Colors.grey;
+const WhiteColor = Colors.white;
+
+const GreenColor = Colors.green;
+const headingTextStyle = TextStyle(fontSize: 30, color: GrayColor, fontWeight: FontWeight.w500);
